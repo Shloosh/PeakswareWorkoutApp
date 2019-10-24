6ea15005bcca6515a935f6ad1549961f4a83eaaf
Brian <bschmoker733@gmail.com>
2019-10-23 20:33:52 -0600

    Expose workout data to API

--- server.js
+++ server.js
@@ -1,11 +1,23 @@
 const express = require('express');
 const path = require('path');
+const fs = require('fs');
+
+var workoutData;
+fs.readFile('./workout-data.json', (err, data) => {
+  if (err) throw err;
+  workoutData = JSON.parse(data);
+});
 
 const app = express();
 
 app.use(
   express.static(path.join(__dirname, '/dist'))
 );
+
+app.get('/api/workoutData', (req, res) => {
+  res.json(workoutData);
+  console.log('Sent workout data');
+});
 
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '/dist/index.html'));
