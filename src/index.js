09d013bd85873f8c7ddad3e33856e8c8789f232b
Brian <bschmoker733@gmail.com>
2019-10-24 20:26:58 -0600

    Add react router to project. Add run script for dev build.

--- src/index.js
+++ src/index.js
@@ -1,10 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+const Root = () => (
+  <Router>
+    <Switch>
+      <Route exact path="/" component={App} />
+      <Route path="/test" component={Test} />
+    </Switch>
+  </Router>
+);
 
 const App = () => (
   <div>
     <h1>Hello World!</h1>
   </div>
-)
+);
 
-ReactDOM.render(<App/>, document.getElementById('root'));
+const Test = () => (
+  <div>
+    <h1>Test page</h1>
+  </div>
+);
+
+ReactDOM.render(<Root/>, document.getElementById('root'));
