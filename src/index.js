aa546bb37c890aeb4460aacff44ef5954b549bcb
Brian <bschmoker733@gmail.com>
2019-10-26 13:17:34 -0600

    Remove minimal leaflet example

--- src/index.js
+++ src/index.js
@@ -3,13 +3,9 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import axios from 'axios';
 import {
-  Circle,
-  CircleMarker,
   Map,
-  Polygon,
   Polyline,
   Popup,
-  Rectangle,
   TileLayer,
   Marker
 } from 'react-leaflet';
@@ -173,52 +169,11 @@ class ZoomLineChart extends React.Component {
   }
 }
 
-const center = [51.505, -0.09]
-
-const polyline = [[51.505, -0.09], [51.51, -0.1], [51.51, -0.12]]
-
-const multiPolyline = [
-  [[51.5, -0.1], [51.5, -0.12], [51.52, -0.12]],
-  [[51.5, -0.05], [51.5, -0.06], [51.52, -0.06]],
-]
-
-const polygon = [[51.515, -0.09], [51.52, -0.1], [51.52, -0.12]]
-
-const multiPolygon = [
-  [[51.51, -0.12], [51.51, -0.13], [51.53, -0.13]],
-  [[51.51, -0.05], [51.51, -0.07], [51.53, -0.07]],
-]
-
-const rectangle = [[51.49, -0.08], [51.5, -0.06]]
-
-class VectorLayersExample extends Component {
-  render() {
-    return (
-      <Map center={center} zoom={13}>
-        <TileLayer
-          attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        />
-        <Circle center={center} fillColor="blue" radius={200} />
-        <CircleMarker center={[51.51, -0.12]} color="red" radius={20}>
-          <Popup>Popup in CircleMarker</Popup>
-        </CircleMarker>
-        <Polyline color="black" positions={polyline} />
-        <Polyline color="black" positions={multiPolyline} />
-        <Polygon color="purple" positions={polygon} />
-        <Polygon color="purple" positions={multiPolygon} />
-        <Rectangle bounds={rectangle} color="black" />
-      </Map>
-    )
-  }
-}
-
 const Root = () => (
   <Router>
     <Switch>
       <Route exact path="/" component={App} />
       <Route path="/map" component={MyMap} />
-      <Route path="/leaflet" component={VectorLayersExample} />
       <Route path="/chart" component={ZoomLineChart} />
     </Switch>
   </Router>
