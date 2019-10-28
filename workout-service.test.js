5ae72bb1f06837e1838418692d4afe3784ee2ef9
Brian <bschmoker733@gmail.com>
2019-10-27 22:23:03 -0600

    Group unit tests appropriately via describe()

--- workout-service.test.js
+++ workout-service.test.js
@@ -1,213 +1,225 @@
 import "babel-polyfill"
 const workoutService = require('./workout-service.js');
 
-it('should format coordinate data correctly', done => {
-  workoutService.getCoordinates(testWorkoutData).then(coordinateData => {
-    expect(coordinateData).toEqual(
-      [
-        {
-          "millisecondOffset": 0
-        },
-        {
-          "millisecondOffset": 1000
-        },
-        {
-          "millisecondOffset": 2000
-        },
-        {
-          "millisecondOffset": 3000
-        },
-        {
-          "millisecondOffset": 4000,
-          "positionLat": 40.01488,
-          "positionLong": -105.131
-        },
-        {
-          "millisecondOffset": 5000,
-          "positionLat": 40.01495,
-          "positionLong": -105.131
-        },
-        {
-          "millisecondOffset": 6000,
-          "positionLat": 40.01503,
-          "positionLong": -105.13099
-        },
-        {
-          "millisecondOffset": 7000,
-          "positionLat": 40.01511,
-          "positionLong": -105.13099
-        },
-        {
-          "millisecondOffset": 8000,
-          "positionLat": 40.01518,
-          "positionLong": -105.13099
-        },
-        {
-          "millisecondOffset": 9000,
-          "positionLat": 40.01525,
-          "positionLong": -105.13099
-        },
-        {
-          "millisecondOffset": 10000,
-          "positionLat": 40.01532,
-          "positionLong": -105.13099
-        }
-      ]
-    );
-    done();
+describe('Workout Service', () => {
+  
+  describe('Coordinate Data', () => {
+    it('should format coordinate data correctly', done => {
+      workoutService.getCoordinates(testWorkoutData).then(coordinateData => {
+        expect(coordinateData).toEqual(
+          [
+            {
+              "millisecondOffset": 0
+            },
+            {
+              "millisecondOffset": 1000
+            },
+            {
+              "millisecondOffset": 2000
+            },
+            {
+              "millisecondOffset": 3000
+            },
+            {
+              "millisecondOffset": 4000,
+              "positionLat": 40.01488,
+              "positionLong": -105.131
+            },
+            {
+              "millisecondOffset": 5000,
+              "positionLat": 40.01495,
+              "positionLong": -105.131
+            },
+            {
+              "millisecondOffset": 6000,
+              "positionLat": 40.01503,
+              "positionLong": -105.13099
+            },
+            {
+              "millisecondOffset": 7000,
+              "positionLat": 40.01511,
+              "positionLong": -105.13099
+            },
+            {
+              "millisecondOffset": 8000,
+              "positionLat": 40.01518,
+              "positionLong": -105.13099
+            },
+            {
+              "millisecondOffset": 9000,
+              "positionLat": 40.01525,
+              "positionLong": -105.13099
+            },
+            {
+              "millisecondOffset": 10000,
+              "positionLat": 40.01532,
+              "positionLong": -105.13099
+            }
+          ]
+        );
+        done();
+      });
+    });
   });
-});
+  
+  describe('Event Data', () => {
+    it('should format event data correctly', done => {
+      workoutService.getEvents(testWorkoutData).then(eventData => {
+        expect(eventData).toEqual(
+          [
+            {
+              "eventType": "start",
+              "millisecondOffset": 0,
+              "values": {
+                "heartRate": 74,
+                "cadence": 63,
+                "power": 143,
+                "temperature": 22,
+                "elevation": 1543.4,
+                "distance": 0,
+                "speed": 7.231
+              }
+            },
+            {
+              "eventType": "stop",
+              "millisecondOffset": 10000,
+              "values": {
+                "heartRate": 82,
+                "cadence": 77,
+                "power": 181,
+                "temperature": 22,
+                "elevation": 1544,
+                "distance": 49.54,
+                "speed": 8.378,
+                "positionLat": 40.01532,
+                "positionLong": -105.13099
+              }
+            }
+          ]
+        );
+        done();
+      });
+    });
+    
+    it('should format rounded event data correctly', done => {
+      workoutService.getEventsRounded(testWorkoutData).then(eventRoundedData => {
+        expect(eventRoundedData).toEqual(
+          [
+            {
+              "eventType": "start",
+              "millisecondOffset": 0,
+              "values": {
+                "positionLat": 40.01488,
+                "positionLong": -105.131
+              }
+            },
+            {
+              "eventType": "stop",
+              "millisecondOffset": 10000,
+              "values": {
+                "heartRate": 82,
+                "cadence": 77,
+                "power": 181,
+                "temperature": 22,
+                "elevation": 1544,
+                "distance": 49.54,
+                "speed": 8.378,
+                "positionLat": 40.01532,
+                "positionLong": -105.13099
+              }
+            }
+          ]
+        );
+        done();
+      });
+    });
+  });
+  
+  describe('Power Output Data', () => {
+    it('should format power output data correctly', done => {
+      workoutService.getPowerOutput(testWorkoutData).then(powerOutputData => {
+        expect(powerOutputData).toEqual(
+          [
+            {
+              "secondOffset": 0,
+              "power": 143
+            },
+            {
+              "secondOffset": 1,
+              "power": 158
+            },
+            {
+              "secondOffset": 2,
+              "power": 157
+            },
+            {
+              "secondOffset": 3,
+              "power": 157
+            },
+            {
+              "secondOffset": 4,
+              "power": 157
+            },
+            {
+              "secondOffset": 5,
+              "power": 24
+            },
+            {
+              "secondOffset": 6,
+              "power": 137
+            },
+            {
+              "secondOffset": 7,
+              "power": 99
+            },
+            {
+              "secondOffset": 8,
+              "power": 134
+            },
+            {
+              "secondOffset": 9,
+              "power": 139
+            },
+            {
+              "secondOffset": 10,
+              "power": 181
+            }
+          ]
+        );
+        done();
+      });
+    });
+  });
 
-it('should format event data correctly', done => {
-  workoutService.getEvents(testWorkoutData).then(eventData => {
-    expect(eventData).toEqual(
-      [
-        {
-          "eventType": "start",
-          "millisecondOffset": 0,
-          "values": {
-            "heartRate": 74,
-            "cadence": 63,
-            "power": 143,
-            "temperature": 22,
-            "elevation": 1543.4,
-            "distance": 0,
-            "speed": 7.231
+  describe('Best Effort Data', () => {
+    it('should format best effort data correctly', done => {
+      workoutService.getBestEffort(testWorkoutData, 1000).then(bestEffort => {
+        expect(bestEffort).toEqual(
+          {
+            "totalPowerOutput": 181,
+            "startTime": 9000,
+            "endTime":10000
           }
-        },
-        {
-          "eventType": "stop",
-          "millisecondOffset": 10000,
-          "values": {
-            "heartRate": 82,
-            "cadence": 77,
-            "power": 181,
-            "temperature": 22,
-            "elevation": 1544,
-            "distance": 49.54,
-            "speed": 8.378,
-            "positionLat": 40.01532,
-            "positionLong": -105.13099
-          }
-        }
-      ]
-    );
-    done();
+        );
+        done();
+      });
+    });
+    
+    it('should calculate the correct best effort within a 3 second timeframe', done => {
+      workoutService.getBestEffort(testWorkoutData, 3000).then(bestEffort => {
+        expect(bestEffort.totalPowerOutput).toEqual(472);
+        done();
+      });
+    });
+    
+    it('should calculate the correct best effort within a 5 second timeframe', done => {
+      workoutService.getBestEffort(testWorkoutData, 5000).then(bestEffort => {
+        expect(bestEffort.totalPowerOutput).toEqual(690);
+        done();
+      });
+    });
   });
-});
 
-it('should format rounded event data correctly', done => {
-  workoutService.getEventsRounded(testWorkoutData).then(eventRoundedData => {
-    expect(eventRoundedData).toEqual(
-      [
-        {
-          "eventType": "start",
-          "millisecondOffset": 0,
-          "values": {
-            "positionLat": 40.01488,
-            "positionLong": -105.131
-          }
-        },
-        {
-          "eventType": "stop",
-          "millisecondOffset": 10000,
-          "values": {
-            "heartRate": 82,
-            "cadence": 77,
-            "power": 181,
-            "temperature": 22,
-            "elevation": 1544,
-            "distance": 49.54,
-            "speed": 8.378,
-            "positionLat": 40.01532,
-            "positionLong": -105.13099
-          }
-        }
-      ]
-    );
-    done();
-  });
-});
-
-it('should format power output data correctly', done => {
-  workoutService.getPowerOutput(testWorkoutData).then(powerOutputData => {
-    expect(powerOutputData).toEqual(
-      [
-        {
-          "secondOffset": 0,
-          "power": 143
-        },
-        {
-          "secondOffset": 1,
-          "power": 158
-        },
-        {
-          "secondOffset": 2,
-          "power": 157
-        },
-        {
-          "secondOffset": 3,
-          "power": 157
-        },
-        {
-          "secondOffset": 4,
-          "power": 157
-        },
-        {
-          "secondOffset": 5,
-          "power": 24
-        },
-        {
-          "secondOffset": 6,
-          "power": 137
-        },
-        {
-          "secondOffset": 7,
-          "power": 99
-        },
-        {
-          "secondOffset": 8,
-          "power": 134
-        },
-        {
-          "secondOffset": 9,
-          "power": 139
-        },
-        {
-          "secondOffset": 10,
-          "power": 181
-        }
-      ]
-    );
-    done();
-  });
-});
-
-it('should format best effort data correctly', done => {
-  workoutService.getBestEffort(testWorkoutData, 1000).then(bestEffort => {
-    expect(bestEffort).toEqual(
-      {
-        "totalPowerOutput": 181,
-        "startTime": 9000,
-        "endTime":10000
-      }
-    );
-    done();
-  });
-});
-
-it('should calculate the correct best effort within a 3 second timeframe', done => {
-  workoutService.getBestEffort(testWorkoutData, 3000).then(bestEffort => {
-    expect(bestEffort.totalPowerOutput).toEqual(472);
-    done();
-  });
-});
-
-it('should calculate the correct best effort within a 5 second timeframe', done => {
-  workoutService.getBestEffort(testWorkoutData, 5000).then(bestEffort => {
-    expect(bestEffort.totalPowerOutput).toEqual(690);
-    done();
-  });
 });
 
 const testWorkoutData = 
